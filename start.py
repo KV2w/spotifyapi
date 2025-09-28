74659ca6612322ecccd33831069b475f060daae6
Kelvin Keller <kwkellerjr2000@gmail.com>
2025-09-28 12:30:08 -0400

    Learning Api Calls

---
+++ start.py
@@ -0,0 +1,33 @@
+import requests
+
+#Authenticate
+client_id = "7591cc9758f94598a53634c391559828"
+client_secret = "3f35759fd5df415c8f8fad85ce9494e9"
+
+url = "https://accounts.spotify.com/api/token"
+
+payload = {
+    "grant_type": "client_credentials",
+    "client_id": client_id,
+    "client_secret": client_secret
+}
+
+headers ={
+    "Content-Type": "JAYR/x-www-form-urlencoded"
+}
+
+try:
+    response = requests.post(url, data=payload, headers=headers)
+    response.raise_for_status()  # Raise an exception for bad status codes (4xx or 5xx)
+
+    token_data = response.json()
+    print("Access token retrieved successfully.")
+    print("Token:", token_data["access_token"])
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
+
+
+# site = requests.get("https://api.spotify.com")
+
+# data = site.json()
+# print(data)
